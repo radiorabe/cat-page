c0a8c94640482cbfdf51bcd772fd262ee5fc11c1
semantic-release <semantic-release>
2019-10-09 17:38:26 +0200

    0.4.0
    Automatically generated by python-semantic-release

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ from werkzeug.middleware.shared_data import SharedDataMiddleware
 from werkzeug.routing import Map, Rule
 from werkzeug.wrappers import Request, Response
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 logger = logging.getLogger("catpage")
 
