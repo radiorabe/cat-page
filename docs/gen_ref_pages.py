11da5089e6361cf3ada5fd218c5b984d8bd0d20a
hairmare <116588+hairmare@users.noreply.github.com>
2024-08-04 12:09:56 +0200

    chore: simplify docs (#480)

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -1,4 +1,4 @@
-"""Generate the code reference pages and navigation.
+"""Generate the docs page.
 
 From https://mkdocstrings.github.io/recipes/
 """
@@ -6,27 +6,6 @@ From https://mkdocstrings.github.io/recipes/
 from pathlib import Path
 
 import mkdocs_gen_files
-
-nav = mkdocs_gen_files.Nav()
-
-path = Path("app/")
-module_path = path.relative_to(".").with_suffix("")
-doc_path = path.relative_to(".").with_suffix(".md")
-full_doc_path = Path("reference", doc_path)
-
-parts = tuple(module_path.parts)
-
-nav[parts] = doc_path.as_posix()
-
-full_doc_path.parent.mkdir(parents=True, exist_ok=True)
-with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-    ident = ".".join(parts)
-    fd.write(f"::: {ident}")
-
-mkdocs_gen_files.set_edit_path(full_doc_path, path)
-
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
-    nav_file.writelines(nav.build_literate_nav())
 
 with Path("README.md").open("r") as readme, mkdocs_gen_files.open(
     "index.md",
